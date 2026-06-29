f5afb71cfeb9e16d7a7e6db30112a99b1c25a9e6
Saivardhan eppalpelli <saieppalpelli@gmail.com>
2026-06-29 15:51:53 +0530

    Add files via upload

---
+++ 4th.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=5;
+    int sum=a+b;
+    int product=a*b;
+    int division=a/b;
+    
+    cout<<sum<<endl;
+    cout<<product<<endl;
+    cout<<division<<endl;
+}
