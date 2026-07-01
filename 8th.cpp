e8feb43a23305fd3fa9037bdb9d84d6854c3d3d6
Saivardhan eppalpelli <saieppalpelli@gmail.com>
2026-07-01 23:59:08 +0530

    Add files via upload

---
+++ 8th.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=-20;
+    if(n>=0){
+        cout <<"n is positive\n";
+    }
+    else{
+        cout <<"n is negetive"<<endl;
+}
+    return 0;
+}
