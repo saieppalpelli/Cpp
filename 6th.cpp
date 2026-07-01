863e0301eb549cf5b3774c0144e0d7481be9a031
Saivardhan eppalpelli <saieppalpelli@gmail.com>
2026-07-01 23:58:16 +0530

    Add files via upload

---
+++ 6th.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the a:"<<endl;
+    cin>>a;
+    cout<<"enter the b:"<<endl;
+    cin>>b;
+    int sum=a+b;
+    cout<<"the sumis:"<<sum<<endl;
+    
+}
