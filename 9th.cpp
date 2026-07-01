a1729c879ea8a335e00f2f43e08bbafbbd0fc937
Saivardhan eppalpelli <saieppalpelli@gmail.com>
2026-07-01 23:59:27 +0530

    Add files via upload

---
+++ 9th.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter the ch:"<<endl;
+    cin>>ch;
+    if(ch>="a"&& ch<="z"){
+    cout<<"uppercase"<<endl;
+    }else{
+        cout<<"lowercase"<<endl;
+
+    }
+}
